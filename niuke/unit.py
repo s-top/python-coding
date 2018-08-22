6a746e68f68db6e16a19af12e3c9bde8329ab536
s-top <15634647@qq.com>
2018-08-22 20:45:57 +0800

    niukes

---
+++ niuke/unit.py
@@ -0,0 +1,15 @@
+import sys
+class Solution:
+    def ReverseNum(self, myList):
+        result = []
+        return result
+
+if __name__ == '__main__':
+    number = int(input())
+    print(number)
+    t = int(sys.stdin.readline())
+    print(t)
+    # resList = []
+    # for i in range(t):
+    #     n = int(sys.stdin.readline())
+    #     array = list(map(int, sys.stdin.readline().strip().split()))
